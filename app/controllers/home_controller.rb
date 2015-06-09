d5be3204cdea3846c4ad2bcb42201bcb8ef626b4
JohnDGreiner <john.d.greiner@gmail.com>
2015-06-09 09:21:11 -0400

    Active record challenge.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,14 @@
 class HomeController < ApplicationController
 
   def form
-    
+
+    @survey = Survey.new
+
+    respond_to do |format|
+      if @survey.save
+        format.html { redirect_to ty }
+      end
+    end
   end
 
 end
