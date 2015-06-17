2e0822f1f244a5a1bc2a0400919b2db9ddf511ca
JohnDGreiner <john.d.greiner@gmail.com>
2015-06-17 09:09:08 -0400

    Add session name.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,12 +2,15 @@ class HomeController < ApplicationController
 
   def form
 
-    @survey = Survey.create!(name: params[:name], chocolate: params[:chocolate], rainbows: params[:rainbows], puppies: params[:puppies], cash: params[:cash],)
-    respond_to do |format|
-      if @survey.save
-        format.html { redirect_to home_show_path }
+    if session[:name] != params[:name]
+      @survey = Survey.create!(name: params[:name], chocolate: params[:chocolate], rainbows: params[:rainbows], puppies: params[:puppies], cash: params[:cash],)
+      respond_to do |format|
+        if @survey.save
+          format.html { redirect_to home_show_path }
+        end
       end
     end
+    session[:name] = params[:name]
   end
 
   def show
